2bb744c31a38641dd3f8e171c4015fadd499c4bf
player31-kks <keumks789@naver.com>
2023-08-29 00:33:35 +0900

    [Add] using combinations at 2309

--- 10_week_finish/1_week/2309.py
+++ 10_week_finish/1_week/2309.py
@@ -1,4 +1,5 @@
 import copy
+from itertools import combinations
 heights = [int(input()) for _ in range(9)]
 heights.sort()
 
@@ -15,10 +16,13 @@ def DFS(start,end,person):
         DFS(i+1,end,person)
         person.pop()
 
+def DFS2():
+    for res in combinations(heights,7):
+        if sum(res)==100:
+            return res
 
-DFS(0,9,[]) 
-for person in result[0]:
-    print(person)
+DFS(0,9,[])
+print(DFS2())
 
 
 
